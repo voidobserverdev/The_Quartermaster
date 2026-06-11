ebecc59a9cfbe6259c4e41c5c50101dd93f2c317
voidobserverdev <void.observer.dev@gmail.com>
2026-06-11 11:10:20 +0530

    Added item filter

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -7,16 +7,36 @@ public class InventoryManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI itemSlot;
     [SerializeField] Transform inventoryContainer;
-    [SerializeField] Button button;
+    [SerializeField] Button generateItemButton;
+    [SerializeField] Button filterWeapon;
+    [SerializeField] Button filterArmor;
+    [SerializeField] Button filterPotion;
+    [SerializeField] Button showAllItems;
     public List<Item> playerInventory = new List<Item>(); // can simply use new()
 
     void Start()
     {
-        button.onClick.AddListener(() =>
+        generateItemButton.onClick.AddListener(() =>
         {
             GenerateRandomItem();
             Debug.Log(playerInventory.Count);
         });
+        filterArmor.onClick.AddListener(() =>
+        {
+            FilterInventory(ItemCategory.Armor);
+        });
+        filterWeapon.onClick.AddListener(() =>
+        {
+            FilterInventory(ItemCategory.Weapon);
+        });
+        filterPotion.onClick.AddListener(() =>
+        {
+            FilterInventory(ItemCategory.Potion);
+        });
+        showAllItems.onClick.AddListener(() =>
+       {
+           RefreshUI();
+       });
     }
 
     void GenerateRandomItem()
@@ -46,6 +66,22 @@ public class InventoryManager : MonoBehaviour
         {
             TextMeshProUGUI spawnedItem = Instantiate(itemSlot, inventoryContainer);
             spawnedItem.text = $"{item.name}-{item.category}-{item.value}g";
+        }
+    }
+
+    void FilterInventory(ItemCategory filterCategory)
+    {
+        foreach (Transform item in inventoryContainer)
+        {
+            Destroy(item.gameObject);
+        }
+        foreach (Item item in playerInventory)
+        {
+            if (filterCategory == item.category)
+            {
+                TextMeshProUGUI spawnedItem = Instantiate(itemSlot, inventoryContainer);
+                spawnedItem.text = $"{item.name}-{item.category}-{item.value}g";
+            }
         }
     }
 }
